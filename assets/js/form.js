7a4f360a7d2e088076bef039b68d2548ba21ffcc
Maxwell <155778466+maxwellmitchell93@users.noreply.github.com>
2024-03-28 23:00:59 -0700

    Light & Dark theme

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,27 +1,9 @@
-const userNameInput = document.querySelector('#user-name');
-const blogTitleInput = document.querySelector('#blog-title');
-const contentInput = document.querySelector('#content');
-const submitButton = document.querySelector('#submit');
-const form = document.querySelector('#form');
-
-//Add event Listener for submit button
-submitButton.addEventListener('click', function (event) {
-    event.preventDefault();
-    // //Blog post data saved in localstorage
-    const blogPost = {
-        userName: userNameInput.value.trim(),
-        blogTitleInput: blogTitleInput.value.trim(),
-        contentInput: contentInput.value.trim(),
-    };
-
-    localStorage.setItem('blogPost', JSON.stringify(blogPost));
-
-});
-//Darkmode
+//Dark mode
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
- }
+}
+
 
 
 
